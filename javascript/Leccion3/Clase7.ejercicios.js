2a0486395c75767f9bcb1d7278cb424efcd563bf
Wanda Lanatta <wandalanatta5bod@gmail.com>
2024-06-04 00:03:45 -0300

    Agregando leccion3 a mi carpeta de Javascript

---
+++ javascript/Leccion3/Clase7.ejercicios.js
@@ -0,0 +1,23 @@
+//Ejercicios para encontrar numeros pares
+//Ejercicio1
+let parImpar = 75;
+if(parImpar % 2 ==0){
+ console.log("Es un numero PAR");
+
+}
+else{
+
+ console.log("Es un numero IMPAR");
+
+}
+
+//Ejercicio2 
+
+let edad = 14 , adulto = 18;
+if (edad >= adulto) {
+    console.log("Es una persona adulta");
+}
+else {
+  console.log("Es un menor de edad");
+}
+
---
+++ javascript/Leccion3/Clase8.ejercicios.js
@@ -0,0 +1,10 @@
+//Ejercicio3
+
+let dentroRango = 32;
+let valMin = 0, valMax =50;
+if(dentroRango >= valMin && dentroRango <= valMax){
+     console.log("Esta dentro del rango establecido");
+}
+else{
+ console.log("Esta fuera del rango establecido");
+}
