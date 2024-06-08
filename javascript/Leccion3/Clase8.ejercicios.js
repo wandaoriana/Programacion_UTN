f728babeb03adae2055fbb2f815daceb881bca22
Wanda Lanatta <wandalanatta5bod@gmail.com>
2024-06-08 00:11:52 -0300

    Agregando Clase 9 de la leccion 3

--- javascript/Leccion3/Clase8.ejercicios.js
+++ javascript/Leccion3/Clase8.ejercicios.js
@@ -1,10 +1,17 @@
-//Ejercicio3
+// //Ejercicio3
 
-let dentroRango = 32;
-let valMin = 0, valMax =50;
-if(dentroRango >= valMin && dentroRango <= valMax){
-     console.log("Esta dentro del rango establecido");
-}
-else{
- console.log("Esta fuera del rango establecido");
-}
+// let dentroRango = 32;
+// let valMin = 0, valMax =50;
+// if(dentroRango >= valMin && dentroRango <= valMax){
+//      console.log("Esta dentro del rango establecido");
+// }
+// else{
+//  console.log("Esta fuera del rango establecido");
+// }
+
+
+//Hoy ya no se usa var, se utiiza let y const
+nombre = "Pedro";
+console.log(nombre); Pedro 
+const apellido = "lepes";
+console.log(apellido);
---
+++ javascript/Leccion3/Clase9.ejercicios.js
@@ -0,0 +1,50 @@
+
+// //Ejercicio : Si el padre puede asisitr al juegoo de su hijo
+// let vacaciones = false, diaDescanso = false;
+// if(vacaciones|| diaDescanso){
+//     console.log("El padre puede asistir al juego de su hijo");
+//    }
+     
+//   else{( "El padre no puede asistir al juego");
+
+
+//    }
+
+
+
+// Operador ternario( para hacer mas eficas nuestro codigo)
+let resulltado2= 3 > 2 ?"Verdadero" ; "Falso";
+console.log(resulltado2);
+let numero = 9;
+resulltado2= numero % 2 == 0? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resulltado2);
+//Convertir string a number
+let miNumero = "21";
+console.log(typeof miNumero); //Es una cadena
+
+if(isNaN(miNumero)){
+  console.log("Esto es una funcion");
+}
+
+if(edad2 >= edad2){
+  console.log("Esta variable no contiene solo numeros");
+}
+
+else{
+  if(edad2 >= 18){
+    console.log("Puede votar");
+  }
+  else{
+    console.log("Muy joven para votar");
+  }
+}
+
+let resulltado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+
+console.log(resultado3);
+
+
+
+
+//Funcion isNam
+
